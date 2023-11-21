94a7fd725ecd81a7e723d60fae930b2dcaeedaec
Muhammad Faqih <freemiumlemper300@gmail.com>
2023-11-21 21:31:34 +0700

    Add Card Preview

--- app/src/main/java/com/example/bringtodo/MainActivity.kt
+++ app/src/main/java/com/example/bringtodo/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.bringtodo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.outlined.DateRange
 import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -23,16 +36,24 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.rememberNavController
 import com.example.bringtodo.ui.theme.BringToDoTheme
 data class IconForNav(
     val title : String,
     val selectedIcon : ImageVector,
-    val unselectedIcon : ImageVector
+    val unselectedIcon : ImageVector,
+    val route : String
 )
+
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -41,32 +62,39 @@ class MainActivity : ComponentActivity() {
         setContent {
             BringToDoTheme {
                 // A surface container using the 'background' color from the theme
+                val navController = rememberNavController()
                 val items = listOf(
                     IconForNav(
                         title = "Acara",
                         selectedIcon = Icons.Default.DateRange,
-                        unselectedIcon = Icons.Outlined.DateRange
+                        unselectedIcon = Icons.Outlined.DateRange,
+                        route = "ListAcara"
                     ),
                     IconForNav(
                         title = "Barang",
                         selectedIcon = Icons.Filled.Edit,
-                        unselectedIcon = Icons.Outlined.Edit
+                        unselectedIcon = Icons.Outlined.Edit,
+                        route = "ListBarang"
                     ),
                 )
                 var selectedItem by rememberSaveable {
-                    mutableStateOf(0)
+                    mutableIntStateOf(0)
                 }
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Scaffold(bottomBar = {
+                    Scaffold(topBar = {
+
+                    },bottomBar = {
                         NavigationBar {
                             items.forEachIndexed{
                                 index,item -> NavigationBarItem(
                                 selected = selectedItem == index,
                                 onClick = {
                                     selectedItem = index
+//                                    navController.navigate(item.route)
                                 }, label = {
                                         Text(text = item.title)
                                 },
@@ -82,7 +110,40 @@ class MainActivity : ComponentActivity() {
                         FloatingActionButton(onClick = { /*TODO*/ }) {
                             Icon(Icons.Default.Add,contentDescription = "Add")
                         }
-                    }) {
+                    }) {innerPadding ->
+                        Column(
+                            modifier = Modifier
+                                .padding(innerPadding)
+                                .verticalScroll(rememberScrollState()),
+                            verticalArrangement = Arrangement.spacedBy(16.dp),
+                        ) {
+                            repeat(30){
+                                Card(
+                                    colors = CardDefaults.cardColors(
+                                        containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                                    ),
+                                    modifier = Modifier
+                                        .height(150.dp)
+                                        .fillMaxWidth()
+                                        .padding(horizontal = 15.dp)
+                                ) {
+                                    Column(modifier = Modifier.padding(horizontal = 20.dp, vertical = 20.dp)) {
+                                        Text(
+                                            text = "Event Name",
+                                            textAlign = TextAlign.Center,
+                                            fontSize = 30.sp
+                                        )
+                                        Text(
+                                            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                                            fontSize = 13.sp,
+                                            modifier = Modifier.padding(start = 0.dp, top = 5.dp)
+                                        )
+
+                                    }
+
+                                }
+                            }
+                        }
 
                     }
                 }
