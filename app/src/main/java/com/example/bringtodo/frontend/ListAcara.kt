d51f139b176f3c95e20fadf339a9e3fcb12994c3
Muhammad Faqih <freemiumlemper300@gmail.com>
2023-11-23 18:03:10 +0700

    Tambah view list acara dan barang

---
+++ app/src/main/java/com/example/bringtodo/frontend/ListAcara.kt
@@ -0,0 +1,88 @@
+package com.example.bringtodo.frontend
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.bringtodo.ui.theme.BringToDoTheme
+
+class ListAcara : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BringToDoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ListAcara()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ListAcara(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .padding(5.dp)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+        repeat(30){
+            Card(
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                ),
+                modifier = Modifier
+                    .height(150.dp)
+                    .fillMaxWidth()
+                    .padding(horizontal = 15.dp)
+            ) {
+                Column(modifier = Modifier.padding(horizontal = 20.dp, vertical = 20.dp)) {
+                    Text(
+                        text = "Event Name",
+                        textAlign = TextAlign.Center,
+                        fontSize = 30.sp
+                    )
+                    Text(
+                        text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                        fontSize = 13.sp,
+                        modifier = Modifier.padding(start = 0.dp, top = 5.dp)
+                    )
+
+                }
+
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview3() {
+    BringToDoTheme {
+        ListAcara()
+    }
+}
