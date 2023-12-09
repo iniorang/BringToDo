722126eae2a432a9b15a025e61a66175501bfa17
Muhammad Faqih <freemiumlemper300@gmail.com>
2023-12-09 16:12:39 +0700

    Tambah Baca data barang

--- app/src/main/java/com/example/bringtodo/backend/controller/BarangController.kt
+++ app/src/main/java/com/example/bringtodo/backend/controller/BarangController.kt
@@ -4,17 +4,23 @@ import com.example.bringtodo.backend.Service.BarangService
 import com.example.bringtodo.backend.model.Barang
 import retrofit2.Call
 import retrofit2.Response
-import javax.security.auth.callback.Callback
+import retrofit2.Callback
 
 class BarangController {
     companion object{
         private var barangService : BarangService = ApiClient.getService(BarangService::class.java)
-        fun getBarangs(callback:(List<Barang>?)->Unit){
+        fun getBarang(callback:(List<Barang>?)->Unit){
             barangService.getBarang().enqueue(object : Callback<List<Barang>>{
-                override fun onResponse(call: Call<List<Barang>>,response: Response: Response<List<Barang>>): Unit =
+                override fun onResponse(call: Call<List<Barang>>,response: Response<List<Barang>>): Unit =
                     if(response.isSuccessful){
                         callback(response.body())
                     }
+                else{
+
+                    }
+                override fun onFailure(call: Call<List<Barang>>, t: Throwable) {
+                    callback(null)
+                }
             })
         }
     }
